57b08adb9a3c7fb0fb25119d3770cb659eaea622
Martyn York <martiny@sfu.ca>
2011-06-12 15:45:02 -0700

    trying again

--- contrib/gen-android-mk.sh
+++ contrib/gen-android-mk.sh
@@ -5,28 +5,27 @@ rm -r ../Build/gen-android-Monocle/jni/*
 cp -r ../Core ../Build/gen-android-Monocle/jni/
 cp -r ../Libraries ../Build/gen-android-Monocle/jni/
 
-pushd ../Build/gen-android-Monocle/jni
+cd ../Build/gen-android-Monocle/jni
 echo "include \$(call all-subdir-makefiles)" 														> Android.mk
 
 echo "APP_PLATFORM			:= android-9"															> Application.mk
 echo "APP_STL				:= stlport_static"														>> Application.mk
 
-pushd Core
+cd Core
 echo "LOCAL_PATH := \$(call my-dir)" 																> Android.mk
 echo 																								>> Android.mk
 echo "include \$(CLEAR_VARS)" 																		>> Android.mk
 echo "LOCAL_MODULE 			:= MonocleCore"															>> Android.mk
 echo "LOCAL_SRC_FILES 		:= `find . -type f -iname '*.cpp' -printf %p\ `"						>> Android.mk
-echo "LOCAL_C_INCLUDES 		:= \$(LOCAL_PATH)/../Libraries"											>> Android.mk #:= `find . -type d -printf \$\(LOCAL_PATH\)\/%f\ ` \\"					>> Android.mk
+echo "LOCAL_C_INCLUDES 		:= \$(LOCAL_PATH)/../Libraries"											>> Android.mk
 echo "LOCAL_CFLAGS 			:= -DMONOCLE_ANDROID -DMONOCLE_OPENGL_ES"								>> Android.mk
 echo "LOCAL_STATIC_LIBRARIES:= tinyxml stbimage android_native_app_glue"							>> Android.mk
 echo "LOCAL_LDLIBS 			:= -lGLESv1_CM -lEGL -lGLESv2 -lOpenSLES -landroid"						>> Android.mk
 echo																								>> Android.mk
 echo "include \$(BUILD_SHARED_LIBRARY)"																>> Android.mk
 echo "\$(call import-module,android/native_app_glue)"												>> Android.mk
-popd
 
-pushd Libraries
+cd ../Libraries
 echo "LOCAL_PATH := \$(call my-dir)" 																> Android.mk
 echo 																								>> Android.mk
 echo "include \$(CLEAR_VARS)" 																		>> Android.mk
@@ -44,7 +43,6 @@ echo "							TinyXML/tinyxmlparser.cpp"											>> Android.mk
 echo "LOCAL_C_INCLUDES 		:= \$(LOCAL_PATH)/TinyXML \$(LOCAL_PATH)/../Core"						>> Android.mk
 echo																								>> Android.mk
 echo "include \$(BUILD_STATIC_LIBRARY)"																>> Android.mk
-popd
 
-popd
+cd ../../../../contrib
 
